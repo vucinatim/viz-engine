cd2a823807231c94df392ee8fe78dbb6ca50fb19
Tim Vucina <tim.vucina@gmail.com>
2024-04-27 13:42:33 +0200

    fix: deleted dependency next-themes

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,16 +1,13 @@
-"use client"
+"use client";
 
-import { useTheme } from "next-themes"
-import { Toaster as Sonner } from "sonner"
+import { Toaster as Sonner } from "sonner";
 
-type ToasterProps = React.ComponentProps<typeof Sonner>
+type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
-
   return (
     <Sonner
-      theme={theme as ToasterProps["theme"]}
+      theme={"dark"}
       className="toaster group"
       toastOptions={{
         classNames: {
@@ -25,7 +22,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
       }}
       {...props}
     />
-  )
-}
+  );
+};
 
-export { Toaster }
+export { Toaster };
